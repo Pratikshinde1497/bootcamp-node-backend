510aa1a31e0e20d1f1f92b2aee2b420f1b037d3c
Pratik Shinde <Pratikshinde1497>
2020-09-01 11:59:14 +0530

    add bootcamp model

---
+++ models/Bootcamp.js
@@ -0,0 +1,104 @@
+const mongoose = require('mongoose');
+
+const BootcampSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'please enter name'],
+    unique: true,
+    trim: true,
+    maxlength: [50, 'name cannot be more than 50 charcters']
+  },
+  slug: String,
+  description: {
+    type: String,
+    required: [true, 'please add description'],
+    maxlength: [500, 'description cannot be more than 500 charcters']
+  },
+  website: {
+    type: String,
+    match: [
+      /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)/,
+      'please use valid URL with http or https'
+    ]
+  },
+  phone: {
+    type:String,
+    maxlength: [20, 'phone number cannot be more than 20 numbers']
+  },
+  email: {
+    type: String,
+    match: [
+      /^[^\s@]+@[^\s@]+\.[^\s@]+$/,
+      'enter valid email'
+    ]
+  },
+  address: {
+    type: String,
+    required: [true, 'please enter your address']
+  },
+  location: {
+    //  GEOJSON point
+    type: {
+      type: String,
+      enum: ['Point'],
+      required: true
+    },
+    coordinates: {
+      type: [Number],
+      required: true,
+      index: '2dsphere'
+    },
+    formattedAddress: String,
+    street: String,
+    city: String,
+    state: String,
+    zipcode: String,
+    country: String,
+    
+  },
+  careers: {
+    type: [String],
+    required: true,
+    enum: [
+      'Web Development',
+      'Mobile Development',
+      'Swift Development',
+      'UI/UX',
+      'Data Science',
+      'Bussiness',
+      'Other'
+    ]
+  },
+  averageRating: {
+    type: Number,
+    min: [1, 'Rating cannot be less than 1'],
+    max: [10, 'Rating cannot be more than 10']
+  },
+  averageCost: Number,
+  photo: {
+    type: String,
+    default: 'no-photo.jpg'
+  },
+  housing: {
+    type: Boolean,
+    default: false
+  },
+  jobAssistance: {
+    type: Boolean,
+    default: false
+  },
+  jobGuarantee: {
+    type: Boolean,
+    default: false
+  },
+  acceptGi: {
+    type: Boolean,
+    default: false
+  },
+  createdAt:  {
+    type: Date,
+    default: Date.now
+  }
+})
+
+module.exports = mongoose.model('Bootcamp', BootcampSchema);
