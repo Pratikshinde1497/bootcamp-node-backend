2924b89eb37845c944f2afeab79d3081a4b92767
Pratik Shinde <Pratikshinde1497>
2020-09-11 16:38:39 +0530

    now admin can add new usergit status

--- controllers/users.js
+++ controllers/users.js
@@ -4,56 +4,61 @@ const ErrorResponce = require('../utils/errorResponce');
 
 // @desc      Get all users
 // @route     GET /api/v1/users
-// @access    Private
+// @access    Private/admin
 exports.getUsers = asyncHandler(async (req, res, next) => {
   res.status(200).json(res.advancedResponce);
 })
 
 // @desc      Get a user
 // @route     GET /api/v1/users/:id
-// @access    Private
+// @access    Private/admin
 exports.getUser = asyncHandler(async (req, res, next) => {
   const user = await User.findById(req.params.id);
   if(!user) {
-    next(new ErrorResponce(`no user found`, 404))
+    return next(new ErrorResponce(`no user found`, 404))
   }
-  else {
-    res.status(200).json({
-      success: true,
-      data: user
-    })
-  }
+  res.status(200).json({
+    success: true,
+    data: user
+  })
+})
+
+// @desc      Add new User
+// @route     POST /api/v1/users
+// @access    Private/admin
+exports.addUser = asyncHandler(async (req, res, next) => {
+  const user = await User.create(req.body);
+  res.status(201).json({
+    success: true,
+    data: user
+  })
 })
 
 // @desc      Update a user
 // @route     PUT /api/v1/users/:id
-// @access    Private
+// @access    Private/admin
 exports.updateUser = asyncHandler(async (req, res, next) => {
   const updated = await User.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true});
   if (!updated) {    
-    next(new ErrorResponce(`user not found with id: ${req.params.id}`, 404));
+    return next(new ErrorResponce(`user not found with id: ${req.params.id}`, 404));
   }
-  else {
-    res.status(200).json({
-      success: true,
-      data: updated
-    })
-  }
+  res.status(200).json({
+    success: true,
+    data: updated
+  })
 })
 
 // @desc      Delete a user
 // @route     DELETE /api/v1/users/:id
-// @access    Private
+// @access    Private/admin
 exports.deleteUser = asyncHandler(async (req, res, next) => {
   const deleted = await User.findById(req.params.id);
   if (!deleted) {
-    next(new ErrorResponce(`user not found with id: ${req.params.id}`, 404));
+    return next(new ErrorResponce(`user not found with id: ${req.params.id}`, 404));
   }
-  else {
-    deleted.remove();
-    res.status(200).json({
-      success: true,
-      data: {}
-    })
-  }
+  deleted.remove();
+  res.status(200).json({
+    success: true,
+    data: {}
+  })
 })
--- routes/users.js
+++ routes/users.js
@@ -1,18 +1,23 @@
 const express = require('express');
 const router = express.Router();
-const { getUser, deleteUser, updateUser, getUsers } = require('../controllers/users');
+const { getUser, deleteUser, updateUser, getUsers, addUser } = require('../controllers/users');
 const { protect, authorize } = require('../middlewares/auth');
 const User = require('../models/User');
 const QueryResponce = require('../middlewares/queryResponce');
 
 
+//  make private/admin
+router.use(protect);
+router.use(authorize('admin'));
+
 //  routes
 router.route('/')
-  .get(protect, authorize('admin'), QueryResponce(User), getUsers);
+  .get(QueryResponce(User), getUsers)
+  .post(addUser);
 
 router.route('/:id')
-  .get(protect, authorize('admin'), getUser)
-  .put(protect, authorize('admin'), updateUser)
-  .delete(protect, authorize('admin'), deleteUser);
+  .get(getUser)
+  .put(updateUser)
+  .delete(deleteUser);
 
 module.exports = router;
