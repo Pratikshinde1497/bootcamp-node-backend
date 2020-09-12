ac237368ae9ab174834459abe9046cbaec6b3720
Pratik Shinde <Pratikshinde1497>
2020-09-12 15:48:46 +0530

    add security to api

--- server.js
+++ server.js
@@ -1,17 +1,21 @@
 const express = require("express");
 const dotenv = require("dotenv");
+const morgan = require('morgan');
+const connectDB = require('./config/database');
+const colors = require('colors');
+const errorHandler = require('./middlewares/error');
+//  security modules
 const mongoSanitize = require('express-mongo-sanitize');
-
+const helmet = require('helmet');
+const xss = require('xss-clean');
+const rateLimit = require('express-rate-limit');
+const hpp = require('hpp');
+//  load routes
 const auth = require('./routes/auth');
 const bootcamps = require("./routes/bootcamps");
 const courses = require("./routes/courses");
 const users = require("./routes/users");
 const reviews = require("./routes/reviews");
-
-const morgan = require('morgan');
-const connectDB = require('./config/database');
-const colors = require('colors');
-const errorHandler = require('./middlewares/error');
 
 //  configure dotenv
 dotenv.config({ path: './config/config.env' });
@@ -27,6 +31,21 @@ app.use(express.json());
 
 //  sanitize data to prevent NO-SQL injection
 app.use(mongoSanitize());
+
+//  set security headers
+app.use(helmet());
+
+//  prevent XSS attack
+app.use(xss());
+
+//  limit requests per device within period of time
+app.use(rateLimit({
+  windowMS: 10 * 60 * 1000,       // 10 min 
+  max: 100                       //  100 requests
+}));
+
+//  prevent http params polution
+app.use(hpp());
 
 //  use morgan only if we are running in development mode
 if(process.env.NODE_ENV === 'development') {
